a826d1d19f786fd784350495ae69df635f486310
Megan Dotto <megan.dotto@gmail.com>
2018-04-20 18:32:59 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -2,7 +2,7 @@ class Student
   attr_accessor :user, :knowledge
 
   def initialize(user)
-    @first_name =first_name
+    @first_name = first_name
     @last_name = last_name
     @knowledge = []
   end
